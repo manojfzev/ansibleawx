fcb6d19dbeef14cece6209930b8b958fa398deda
Manoj Deemantha <manojfzev@gmail.com>
2023-10-22 01:26:29 +0530

    Update ineventory file

--- inventory/inventory-v3.py
+++ inventory/inventory-v3.py
@@ -18,41 +18,39 @@ ssh_username = "ansible"
 ssh_private_key_file = os.environ.get('ansible_private_key_file')
 ssh_timeout = 5  # Set the SSH timeout in seconds
 
-# Initialize an empty inventory
-# inventory = {
-#     "_meta": {
-#         "hostvars": {}
-#     },
-#     "webservers": {
-#         "hosts": []
-#     }
-# }
+#Initialize an empty inventory
+inventory = {
+    "_meta": {
+        "hostvars": {}
+    },
+    "webservers": {
+        "hosts": []
+    }
+}
 
 # Function to check if the environment variable exists on a host
 def environment_variable_exists(host):
- #   try:
-        print(ssh_private_key_file)
-    #     cmd = ["ssh", "-i", ssh_private_key_file, "-o", "StrictHostKeyChecking=no", "-o", "ConnectTimeout=" + str(ssh_timeout), ssh_username + "@" + host, "echo $" + desired_environment_variable]
-    #     output = subprocess.check_output(cmd, timeout=ssh_timeout)
-    #     return output.decode().strip().lower()
-    # except subprocess.CalledProcessError:
-    #     return False
-    # except subprocess.TimeoutExpired:
-    #     return False
+    try:        
+        cmd = ["ssh", "-i", ssh_private_key_file, "-o", "StrictHostKeyChecking=no", "-o", "ConnectTimeout=" + str(ssh_timeout), ssh_username + "@" + host, "echo $" + desired_environment_variable]
+        output = subprocess.check_output(cmd, timeout=ssh_timeout)
+        return output.decode().strip().lower()
+    except subprocess.CalledProcessError:
+        return False
+    except subprocess.TimeoutExpired:
+        return False
 
-# # Iterate through the IP range and check for hosts
-# for ip in ip_range.hosts():
-#     host = str(ip)
-#     try:
-#         socket.gethostbyaddr(host)
-#         if environment_variable_exists(host):
-#             inventory["webservers"]["hosts"].append(host)
-#             inventory["_meta"]["hostvars"][host] = {
-#                 "ansible_ssh_user": ssh_username
-#             }
-#     except (socket.herror, TimeoutError):
-#         pass
+# Iterate through the IP range and check for hosts
+for ip in ip_range.hosts():
+    host = str(ip)
+    try:
+        socket.gethostbyaddr(host)
+        if environment_variable_exists(host):
+            inventory["webservers"]["hosts"].append(host)
+            inventory["_meta"]["hostvars"][host] = {
+                "ansible_ssh_user": ssh_username
+            }
+    except (socket.herror, TimeoutError):
+        pass
 
-# # Print the JSON representation of the inventory
-# print(json.dumps(inventory, indent=2))
-environment_variable_exists("10.138.0.2")
+# Print the JSON representation of the inventory
+print(json.dumps(inventory, indent=2))
